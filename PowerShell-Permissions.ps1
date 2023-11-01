f7d1509f7f01205f9f166d20f536ccbfe28b6d52
EstebanMqz <emarquez1895@gmail.com>
2023-10-31 21:03:18 -0600

    System_Environments.bat

--- PowerShell-Permissions.ps1
+++
@@ -1,3 +0,0 @@
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{ Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"& {Start-Process PowerShell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%~f0""""'}&" -Verb RunAs; exit } 
-Get-Process powershell | Where-Object {$_.MainWindowTitle -ne ""} | Stop-Process ; Get-Process -Id (Get-WmiObject Win32_Process -Filter "Name='powershell.exe' and ProcessId=$PID").ParentProcessId
